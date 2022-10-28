d3150e174abb28cafdf590d281a601655bc1a294
Adam Sadowski <asebsadowski@gmail.com>
2022-10-28 16:45:05 -0400

    INIT website

---
+++ R/main.R
@@ -0,0 +1,44 @@
+library(blogdown)
+
+## Always run serve_site to start the Hugo server
+blogdown::serve_site()
+## Show in new window
+
+## Make a new post and an Rmarkdown file will open
+## Make changes to the file, and Knit
+## Knitted changes will show automatiicaly in Viewer
+## and in browser (from Show in new window)
+blogdown::new_post(title = "Hi Dooood",
+                   ext = '.Rmarkdown',
+                   subdir = "post")
+
+blogdown::config_Rprofile()
+blogdown::check_gitignore()
+blogdown::check_content()
+
+
+## This site was first made by
+# new_site(theme = "wowchemy/starter-academic")
+## Then some config...
+# blogdown::config_Rprofile()
+# options(
+#   # to automatically serve the site on RStudio startup, set this option to TRUE
+#   blogdown.serve_site.startup = FALSE,
+#   # to disable knitting Rmd files on save, set this option to FALSE
+#   blogdown.knit.on_save = FALSE     <- change
+#   blogdown.author = "Alison Hill",  <- add
+#   blogdown.ext = ".Rmarkdown",      <- add
+#   blogdown.subdir = "post"          <- add
+# )
+
+## Then some config for Git
+# file.edit(".gitignore")
+
+# Add this content:
+#
+#   .Rproj.user
+# .Rhistory
+# .RData
+# .Ruserdata
+# .DS_Store
+# Thumbs.db
---
+++ content/post/2022-10-28-hi-dooood/index.markdown
@@ -0,0 +1,19 @@
+---
+title: Hi Dooood
+author: Adam Sadowski
+date: '2022-10-28'
+slug: hi-dooood
+categories: []
+tags: []
+subtitle: ''
+summary: ''
+authors: []
+lastmod: '2022-10-28T16:17:59-04:00'
+featured: no
+image:
+  caption: ''
+  focal_point: ''
+  preview_only: no
+projects: []
+---
+summary(Orange)
